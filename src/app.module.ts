7d1f7a47146620e0ad804c168cf862592deeef47
hsazzad-prog <hsazzad02@gmail.com>
2023-04-30 11:55:45 +0600

    deploy in railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,10 @@ import { join } from 'path';
 @Module({
   imports: [AdminModule, ManagerModule, TypeOrmModule.forRoot(
    { type: 'postgres',
-    host: 'postgresql://postgres:jMj4F2prfxt39RL2I3MY@containers-us-west-144.railway.app:7368/railway',
-    port: 7368,
+    host: 'containers-us-west-193.railway.app',
+    port: 6971,
     username: 'postgres',
-    password: 'jMj4F2prfxt39RL2I3MY',
+    password: 'lZhDf6qNBTZVBlEVn9Xm',
     database: 'railway',
     autoLoadEntities: true,
     synchronize: true,
